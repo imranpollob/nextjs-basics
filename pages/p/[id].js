3d428e5b39d313618e073997e43dbc004896d8b5
pollmix <polboy777@gmail.com>
2019-10-11 22:35:49 +0600

    remove HeadTag

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,13 +1,16 @@
 import { useRouter } from "next/router";
 import Layout from "./../../components/Layout";
-import HeadTag from "./../../components/HeadTag";
+import Head from "next/head";
 
 export default function Post() {
   const router = useRouter();
 
   return (
     <>
-      <HeadTag title={router.query.id} />
+      <Head>
+        <title>About is Here</title>
+        <meta name="description" content={router.query.id} />
+      </Head>
       <Layout>
         <h1>{router.query.id}</h1>
         <p>Oh content</p>
