86b0e1fa1478e428e869a55fe1289ab97f4f0eb1
pollmix <polboy777@gmail.com>
2019-10-11 18:53:20 +0600

    _document added

--- components/HeadTag.js
+++ components/HeadTag.js
@@ -8,10 +8,6 @@ export default function HeadTag({ title }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
         <title>{title}</title>
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/nprogress@0.2.0/nprogress.css"
-        ></link>
       </Head>
     </div>
   );
